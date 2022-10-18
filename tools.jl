e7166ad7ff85c93eae6ee17b9d7ab721365a140b
Evan Arsenault <evan.arsenault85@gmail.com>
2022-10-18 15:29:48 -0400

    Up to date now.

--- tools.jl
+++ tools.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra, Random, Distributions
+using LinearAlgebra, Random, Distributions, StatsBase
 
 function environment(x, u, w, pars)
     return x + (pars["D"] - I) * (pars["r"] .* sqrt.(x) .* u) .+ (pars["B"] * w')
@@ -13,6 +13,15 @@ function base_setup()
     sigma = 0.25 * [1 1 1 1 1]
 
     return Dict("D" => D, "B" => B, "r" => r, "mu" => mu, "sg" => sigma)
+end
+
+#this returns the number of times the grid interval (in each dimension), is multipled to reach the rounded version of x_bd
+#hence the index of these values in the grid is just the return value + 1
+function get_index(x, upp, low, n)
+    x_bd = min.(max.(x, low), upp)
+
+    nvals = n ./ (upp - low)
+    return map(x -> round(Int,x), x_bd .* nvals) .+ 1
 end
 
 function main()
@@ -30,6 +39,9 @@ function main()
 
     x = environment(x_0, u, w, pars)
 
-    println(size(x))
-    println(x[:,1])
+    upp = [2.0, 2.0, 2.0]
+    low = [0.0, 0.0, 0.0]
+    n = [20, 20, 20]
+
+    return get_index(x, upp, low, n)
 end
