7c97be35fb4298cb0d3f34143ad00f2111d45109
Evan Arsenault <evan.arsenault85@gmail.com>
2022-10-20 14:58:30 -0400

    VI algorithm in progress

--- tools.jl
+++ tools.jl
@@ -4,6 +4,10 @@ function environment(x, u, w, pars)
     return x + (pars["D"] - I) * (pars["r"] .* sqrt.(x) .* u) .+ (pars["B"] * w')
 end
 
+function c(x,u)
+    return x + u
+end
+
 function base_setup()
     D = [0 1 0.5; 0 0 0.5; 0 0 0]
     r = [1; 1; 2]
@@ -13,6 +17,16 @@ function base_setup()
     sigma = 0.25 * [1 1 1 1 1]
 
     return Dict("D" => D, "B" => B, "r" => r, "mu" => mu, "sg" => sigma)
+end
+
+function expectation(f, x, upp, low, n)
+    ndim, m = size(x)
+
+    #get a set of (possibly non-unique) CartesianIndices
+    index = get_index(x, upp, low, n)
+    index_cart = CartesianIndex.([index[i,:] for i in 1:ndim]...)
+
+    return (1 / m) * sum(f[index_cart])
 end
 
 #this returns the number of times the grid interval (in each dimension), is multipled to reach the rounded version of x_bd
@@ -38,10 +52,38 @@ function main()
     u = [0; 0; 0]
 
     x = environment(x_0, u, w, pars)
+    println(size(x))
 
     upp = [2.0, 2.0, 2.0]
     low = [0.0, 0.0, 0.0]
     n = [20, 20, 20]
 
+    grid = (upp - low) ./ n
     return get_index(x, upp, low, n)
+
+    #VALUE ITERATION
+    #initialize V as the zero function
+    V = zeros((n .+ 1)...) 
+    X =  collect(Iterators.product([[0.0 + grid[i]*j for j in 0:n[i]] for i in 1:length(n)]...))
+
+    #... is the "splat" operator, unpacks the n .+ 1 
+    #(by default, our grid convention includes left, right endpoints)
+
+    delta = 1.0
+    tol = 0.001
+
+    while delta >= tol
+        #set delta = 0
+        delta = 0
+
+        #loop over all states
+        for i in 1:length(V)
+            v = V[i]
+            x = X[i]
+
+            #perform Bellman update
+
+            delta = max(delta, abs(v - V[i]))
+        end
+    end
 end
