b1c074949ade868935de75545473e6e8f244f56b
Evan Arsenault <evan.arsenault85@gmail.com>
2023-05-25 16:25:58 -0400

    Refactored experiment setup to support looping over s

--- init_experiment.jl
+++ init_experiment.jl
@@ -8,10 +8,11 @@ using HDF5
 #define experiment name, scenario code, method
 name = "exp_test.h5"
 scenario_code = "water3x3"
-method = "exact"
+method = "test"
+kernel = "null" #if not using a kernel method, leave as "null" (empty valued attributes aren't supported)
 
 #(all dummy values for now)!!
-#define horizon N, size of C
+#define horizon N, size of C as integers
 N = 10
 size_C = 1000
 
@@ -21,26 +22,42 @@ Xlim = hcat([0.0, 10.0], [0.0, 10.0], [0.0, 10.0])
 Ulim = hcat([0.0, 1.0], [0.0, 1.0], [0.0, 1.0])
 Zlim = [0.0, 5.0]
 
+#how many grid points in Z do we allocate in the file system? number of times VI is run
+#we'll have z_grid + 1 vfuncs in the end, but the result for s == bar c is identically zero 
+s_num = 20 
+s_vals = collect(range(Zlim[1], Zlim[2], s_num + 1))
+
 #define initial value function
 #J_init should have dimension = dimX + 1
 J_init = zeros((10,10,10,5)) 
 
 #open the file in write-mode allows us to overwrite if the file exists
-file = h5open("data/"*name, "w")
+h5open("data/"*name, "w") do file
+    #create group and write in initial data
+    g_init = create_group(file, "init")
+    g_init["size_C"] = size_C
+    g_init["Xlim"] = Xlim
+    g_init["Ulim"] = Ulim
+    g_init["Zlim"] = Zlim
 
-#create group and write in initial data
-g_init = create_group(file, "init")
-g_init["N"] = N
-g_init["size_C"] = size_C
-g_init["Xlim"] = Xlim
-g_init["Ulim"] = Ulim
-g_init["Zlim"] = Zlim
-g_init["J_init"] = J_init
+    file["progress"] = "s1/J"*string(N)
+    file["complete"] = "no"
 
-#write attributes to the file
-attributes(file)["scenario_code"] = scenario_code
-attributes(file)["method"] = method
+    #create a group for each s-value
+    for i in 1:(s_num + 1)
+        s = s_vals[i]
 
-close(file)
+        g = create_group(file, "s"*string(i))
+        g["J"*string(N)] = max.(J_init .- s, 0.0)
+        attributes(g)["value"] = s
+    end
+
+    #write attributes to the file
+    attributes(file)["scenario_code"] = scenario_code
+    attributes(file)["method"] = method
+    attributes(file)["kernel"] = kernel
+    attributes(file)["s_num"] = s_num
+    attributes(file)["N"] = N
+end
 
 println("Experiment "*name*" initialized.")
--- value_iteration.jl
+++ value_iteration.jl
@@ -6,59 +6,77 @@ include("bellman.jl")
 (ii) Data is loaded and stored  in a corresponding HDF5 file, generated in init_experiment.jl.
 (iii) Bellman operators are loaded from bellman.jl.
 =#
+function parse_prog(p)
+    return map(s -> parse(Int64,s[2:end]), split(p,"/"))
+end
 
 #basic data for testing
 name = "exp_test.h5"
+println(h5read("data/exp_test.h5","progress"))
 
 #if value iteration was half-way completed, start at the latest iterate
 #must ensure experiment file has been initialized, however
 if isfile("data/"*name)
     #open the file, access base attributes
-    file = h5open("data/"*name,"r")
-    method = read_attribute(file,"method")
-    scenario_code = read_attribute(file,"scenario_code")
-
-    if "vfuncs" in keys(file)
-        println("Initialized experiment located.")
-        func_strings = keys(file["vfuncs"])
-    
-        #find the most recent iterate (i.e., smallest t)
-        t_min = minimum(map(v -> parse(Int64,v[2:end]), func_strings))
-        J = read(file["vfuncs"], "J"*string(t_min))
-        close(file)
-    else
-        println("Initialized experiment located.")
-        t_min = read(file["init"], "N")
-        J = read(file["init"], "J_init")
-        close(file)
-        
-        println("Saving iterate J"*string(N)*".")
-        h5write("data/"*name, "vfuncs/J"*string(N), J)
+    h5open("data/"*name,"r") do file
+        method = read_attribute(file, "method")
+        scenario_code = read_attribute(file, "scenario_code")
+        s_num = read_attribute(file, "s_num")
+        N = read_attribute(file, "N")
     end
 else
     error("Experiment not initialized.")
 end
 
+#read current progress, completion state
+progress = h5read("data/"*name, "progress")
+complete = h5read("data/"*name, "complete")
+
+#set final condition
+final_cond = "s"*string(s_num)*"/J0"
+
+#read the most recent value function
+J = h5read("data/"*name, progress)
+
 #choose which type of Bellman update to use, and identify relevant parameters
 if method == "exact"
     bellman = bellman_exact
     parameters = h5read("data/"*name, "init")
-    delete!(parameters,"J_init")
 else
     bellman = bellman_test
     parameters = h5read("data/"*name, "init")
-    delete!(parameters,"J_init")
 end
 
 #begin value iteration
-for t=(t_min-1):-1:0
-    #dummy version of Bellman update
-    J_new = bellman(J, scenario_code, parameters)
-    global J = J_new
-    
-    println("Bellman Update complete.")
+if complete == "yes"
+    println("Experiment is complete.")
+else
+    while complete == "no"
+        if progress == final_cond
+            #this means process is complete
+            println("Saving iterate s"*string(s_num + 1)*"/J0.")
+            h5write("data/"*name, "s"*string(s_num + 1)*"/J0", zeros(size(J)))
+            println("Iteration complete for  s"*string(s_num + 1)*".")
+            global complete = "yes"
 
-    println("Saving iterate J"*string(t)*".")
-    h5write("data/"*name, "vfuncs/J"*string(t), J)
+        elseif progress[end-1:end] == "J0"
+            #this means we've finished VI for particular s (not final)
+            #move to next s-value
+            local s, j = parse_prog(progress)
+            println("Iteration complete for s"*string(s)*".")
+            global progress = "s"*string(s+1)*"/J"*string(N)
+            global J = h5read("data/"*name, progress)
+
+        else
+            #move to next iterate for same s
+            local s, j = parse_prog(progress)
+            J_new = bellman(J, scenario_code, parameters)
+            global J = J_new
+            println("Bellman Update complete.")
+            global progress = "s"*string(s)*"/J"*string(j-1)
+            println("Saving iterate "*progress)
+            h5write("data/"*name, progress, J)
+        end
+    end
 end
 
